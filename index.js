618f4058d58160254292f423ddfe88121d5d60f6
unknown <mrcplabon@gmail.com>
2025-12-19 21:45:14 +0600

    Finally

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const stripe = require('stripe')(process.env.STRIPE_SECURE);
 
 const port = process.env.PORT || 3000
 
+
+function generateTrackingId() {
+  const prefix = 'TRK';
+  const timestamp = Date.now().toString(36).toUpperCase();
+  const random = Math.random().toString(36).substring(2, 7).toUpperCase();
+
+  return `${prefix}-${timestamp}-${random}`;
+}
+
+module.exports = generateTrackingId;
+
 //middleware
 app.use(express.json());
 app.use(cors());
@@ -28,6 +39,8 @@ async function run() {
 
     const db = client.db('garments_products_db');
     const productsCollection = db.collection('products');
+    const myOrdersCollection = db.collection('myOrders');
+    const paymentCollection = db.collection('payments')
 
     app.get('/products', async (req, res) => {
       const { limit = 0 } = req.query;
@@ -49,8 +62,6 @@ async function run() {
       const result = await productsCollection.insertOne(product);
       res.send(result);
     })
-
-    const myOrdersCollection = db.collection('myOrders');
 
     app.get('/my-orders', async (req, res) => {
 
@@ -92,48 +103,100 @@ async function run() {
       res.send(result)
     })
 
+
+
     app.post('/create-checkout-session', async (req, res) => {
-      const paymentInfo = req.body;
-      const amount = parseInt(paymentInfo.cost)*100;
+      const { cost, productId, productName, email } = req.body;
+
+      const amount = parseInt(cost) * 100;
+
       const session = await stripe.checkout.sessions.create({
         line_items: [
           {
-            price_data : {
-              currency : 'USD',
-              unit_amount : amount,
-              product_data : {
-                name : paymentInfo.productName,
+            price_data: {
+              currency: 'usd',
+              unit_amount: amount,
+              product_data: {
+                name: productName,
               },
             },
             quantity: 1,
           },
         ],
-        customer_email : paymentInfo.email,
         mode: 'payment',
-        metadata : {
-          productID : paymentInfo.productId,
+        customer_email: email,
+        metadata: {
+          productID: productId,
+          productTitle: productName,
         },
-        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${process.env.SITE_DOMAIN}/dashboard/payment-cancelled`,
       });
-      console.log(session)
-      res.send({ url : session.url });
+
+      res.send({ url: session.url });
+    });
+
+    app.patch('/payment-success', async (req, res) => {
+      const sessionId = req.query.session_id;
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+
+      const transactionId = session.payment_intent ; 
+      const trackingID = generateTrackingId();
+
+      const query = {
+        transactionId : transactionId,
+      }
+
+      const paymentExist = await paymentCollection.findOne(query);
+
+      if(paymentExist){
+        return res.send({message: 'Already exists', transactionId, trackingId:trackingID})
+      }
+
+      if (session.payment_status === 'paid') {
+        const id = session.metadata.productID;
+        await myOrdersCollection.updateOne(
+          { _id: new ObjectId(id) },
+          { $set: { paymentStatus: 'paid', trackingId: trackingID } }
+        );
+
+        const payment = {
+          amount: session.amount_total / 100,
+          currency: session.currency,
+          customerEmail: session.customer_email,
+          productId: session.metadata.productID,
+          productName: session.metadata.productTitle,
+          transactionId: session.payment_intent,
+          paymentStatus: session.payment_status,
+          paidAt: new Date(),
+          trackingId: trackingID,
+        }
+
+        try {
+          await paymentCollection.insertOne(payment);
+          res.send({ success: true, trackingId: trackingID, transactionID: transactionId });
+        } catch (err) {
+          console.error("Payment insert failed:", err);
+          res.status(500).send({ error: "Payment record failed" });
+        }
+      }
+
     })
 
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // // Ensures that the client will close when you finish/error
-      // await client.close();
-    }
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // // Ensures that the client will close when you finish/error
+    // await client.close();
   }
+}
 run().catch(console.dir);
 
-  app.get('/', (req, res) => {
-    res.send('Yay No ones shifting')
-  })
+app.get('/', (req, res) => {
+  res.send('Yay No ones shifting')
+})
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
